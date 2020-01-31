abaf0bec6aeba798e957d9f38588421545421b8b
Ken Dale <ken@kendaleiv.com>
2020-01-31 08:45:54 -0500

    Add initial setup

---
+++ build.cmd
@@ -0,0 +1,9 @@
+@echo Off
+pushd %~dp0
+setlocal enabledelayedexpansion
+
+rmdir /s /q "artifacts"
+
+dotnet build --configuration Release
+dotnet test --configuration Release --no-build
+dotnet pack --configuration Release --no-build --output artifacts
